4a0f30a290beae5069c4b49e645b2e2dd620f393
mariuzzu75 <mariuzzu75@gmail.com>
2020-01-16 11:42:14 +0100

    Add files via upload

---
+++ MCD.py
@@ -0,0 +1,53 @@
+a=int(input("inserisci il numero a: "))
+b=int(input("inserisci il numero b: "))
+
+
+def MCD(a,b):
+    if b==0:
+        print("l'MCD è ",a)
+    else:
+        while b>1:
+            r=a%b
+            
+            if r==0:
+                print("l'MCD è ",b)
+                break
+            else:
+                a=b
+                b=r
+        else:
+            print("l'MCD è 1")
+
+MCD(a,b)
+
+                
+
+
+
+
+
+
+
+
+
+
+            
+            
+    
+
+
+
+
+
+
+
+
+    
+    
+    
+        
+    
+
+        
+
+              
